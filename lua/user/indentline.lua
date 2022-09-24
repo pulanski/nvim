6ff2a22280261819a9c58830ad009692fa4acfdb
pulanski <joshuakersey50@gmail.com>
2022-09-24 01:58:57 -0400

    feat(git-blame): add git-blame plugin
    add git-blame plugin via packer

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -3,12 +3,15 @@ if not status_ok then
     return
 end
 
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]] -- red
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]] -- yellow
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]] --
 vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+
+vim.cmd [[highlight IndentBlanklineIndent7 guibg=#1f1f1f gui=nocombine]] -- gray
+vim.cmd [[highlight IndentBlanklineIndent8 guibg=#1b1b1b gui=nocombine]] -- dark gray
 
 indent_blankline.setup {
     char = "▏",
@@ -22,6 +25,10 @@ indent_blankline.setup {
         "IndentBlanklineIndent4",
         "IndentBlanklineIndent5",
         "IndentBlanklineIndent6",
+    },
+    space_char_highlight_list = {
+        "IndentBlanklineIndent7",
+        "IndentBlanklineIndent8",
     },
     show_current_context = true,
     buftype_exclude = { "terminal", "nofile" },
