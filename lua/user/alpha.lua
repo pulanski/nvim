f012c7eb2bc02294ee17ab9d29772514e9b5f476
pulanski <joshuakersey50@gmail.com>
2022-09-24 01:38:08 -0400

    feat(indent): add indentline blankline highlighting
    add highlighting for indentline blank lines along with a few adjustments to toggleterm

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -1,56 +1,81 @@
 local status_ok, alpha = pcall(require, "alpha")
 if not status_ok then
-  return
+    return
 end
 
 local icons = require "user.icons"
 
-local dashboard = require "alpha.themes.dashboard"
+local status_ok, dashboard = pcall(require, "alpha.themes.dashboard")
+if not status_ok then
+    return
+end
+-- 
+--               R RRRRRRRR R          R   
+--  R RR       R RRRRRRRRRRRRR R      RR   
+-- rR RRR    R RRRRRRRRRRRRRRRRR R   RRR R 
+-- n)
+-- RRR RR   RRRRRRRRRRRRRRRRRRRRRRR  RRRRR 
+--  RRRRR  RRRRRRRRRRRRRRRRRRRRRRRR  RRRR  
+--   RRR RRRRRRRRRRRRRRRRRRRRRRRRRRRR RR   
+--     R  RRRRRRRRRR=  RR = RRRRRRRRRRR    
+--      RRRRRRRRRRRR=  RR = RRRRRRRRRR     
+--       RRRRRRRRRRR   RR   RRRRRRRRRR     
+--      RR==RRRRRRRRRRRRRRRRRRRRRR===RR    
+--      RR =  ==RRRRRRR  RRRRRR==  = RR    
+--       RR =     ===========     = RR     
+--        RR                        R      
+--         R                       R       
+--          R                              
+
+
+
 dashboard.section.header.val = {
-  -- [[                             ..]],
-  -- [[                          .:+:-.]],
-  -- [[                        ..::*=:..]],
-  -- [[                     ....::####:...]],
-  -- [[                  ...-=::\-\-*##=-::::=.]],
-  -- [[                ....::##############=.]],
-  -- [[          ........::#@#############@:.]],
-  -- [[        ..:%::=::::-#################%]],
-  -- [[      ....::*#####=##################+]],
-  -- [[  ......:==%#########################.]],
-  [[:::#:===+##########################-:.]],
-  [[   .....:--%#########################:]],
-  [[       ...::*#####-##################+]],
-  [[         .:=::-::::=#################@]],
-  [[          .........:#+#############+:.]],
-  [[                ....::############%@+.]],
-  [[   +----------+    ..:-:::-###=:::..:.]],
-  [[   | pulanski |      ....::###%:...]],
-  [[   +----------+         ...:-=:..]],
-  [[                          .:%::.]],
+    -- [[                             ..]],
+    -- [[                          .:+:-.]],
+    -- [[                        ..::*=:..]],
+    -- [[                     ....::####:...]],
+    -- [[                  ...-=::\-\-*##=-::::=.]],
+    -- [[                ....::##############=.]],
+    -- [[          ........::#@#############@:.]],
+    -- [[        ..:%::=::::-#################%]],
+    -- [[      ....::*#####=##################+]],
+    -- [[  ......:==%#########################.]],
+
+
+    [[:::#:===+##########################-:.]],
+    [[   .....:--%#########################:]],
+    [[       ...::*#####-##################+]],
+    [[         .:=::-::::=#################@]],
+    [[          .........:#+#############+:.]],
+    [[                ....::############%@+.]],
+    [[                   ..:-:::-###=:::..:.]],
+    [[                     ....::###%:...]],
+    [[                        ...:-=:..]],
+    [[                          .:%::.]],
 }
 
 dashboard.section.buttons.val = {
-  dashboard.button("f", icons.documents.Files .. " Find file", ":Telescope find_files <CR>"),
-  dashboard.button("e", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button(
-    "p",
-    icons.git.Repo .. " Find project",
-    ":lua require('telescope').extensions.projects.projects()<CR>"
-  ),
-  dashboard.button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
-  dashboard.button("t", icons.ui.List .. " Find text", ":Telescope live_grep <CR>"),
-  dashboard.button("s", icons.ui.SignIn .. " Find Session", ":silent Autosession search <CR>"),
-  dashboard.button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
-  dashboard.button("u", icons.ui.CloudDownload .. " Update", ":PackerSync<CR>"),
-  dashboard.button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
+    dashboard.button("f", icons.documents.Files .. " Find file", ":Telescope find_files <CR>"),
+    dashboard.button("n", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
+    -- dashboard.button(
+    --   "p",
+    --   icons.git.Repo .. " Find project",
+    --   ":lua require('telescope').extensions.projects.projects()<CR>"
+    -- ),
+    dashboard.button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
+    dashboard.button("g", icons.ui.List .. " Live Grep", ":Telescope live_grep <CR>"),
+    -- dashboard.button("s", icons.ui.SignIn .. " Find Session", ":silent Autosession search <CR>"),
+    dashboard.button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+    dashboard.button("u", icons.ui.CloudDownload .. " Update", ":PackerSync<CR>"),
+    dashboard.button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
 }
 local function footer()
-  -- NOTE: requires the fortune-mod package to work
-  -- local handle = io.popen("fortune")
-  -- local fortune = handle:read("*a")
-  -- handle:close()
-  -- return fortune
-  return "pulanski"
+    -- NOTE: requires the fortune-mod package to work
+    -- local handle = io.popen("fortune")
+    -- local fortune = handle:read("*a")
+    -- handle:close()
+    -- return fortune
+    return "pulanski"
 end
 
 dashboard.section.footer.val = footer()
--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -1,40 +1,63 @@
 local status_ok, toggleterm = pcall(require, "toggleterm")
 if not status_ok then
-	return
+    return
 end
 
-toggleterm.setup({
-	size = 20,
-	open_mapping = [[<c-\>]],
-	hide_numbers = true,
-	shade_terminals = true,
-	shading_factor = 2,
-	start_in_insert = true,
-	insert_mappings = true,
-	persist_size = true,
-	direction = "float",
-	close_on_exit = true,
-	shell = vim.o.shell,
-	float_opts = {
-		border = "curved",
-	},
-})
+toggleterm.setup {
+    size = 8,
+    open_mapping = [[<leader>t]],
+    hide_numbers = true,
+    shade_terminals = true,
+    shading_factor = 1,
+    start_in_insert = true,
+    insert_mappings = true,
+    persist_size = true,
+    direction = "horizontal",
+    close_on_exit = true,
+    shell = vim.o.shell,
+    float_opts = {
+        border = "curved",
+    },
+}
 
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  -- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+    local opts = { noremap = true }
+    -- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
 end
 
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+local lazygit = Terminal:new { cmd = "lazygit", hidden = true, direction = "float" }
 
 function _LAZYGIT_TOGGLE()
-	lazygit:toggle()
+    lazygit:toggle()
 end
 
+vim.api.nvim_set_keymap("n", "<leader>l", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", { noremap = true, silent = true })
+
+-- create a new terminal with lazydocker
+
+local lazydocker = Terminal:new { cmd = "lazydocker", hidden = true, direction = "float" }
+
+function _LAZYDOCKER_TOGGLE()
+    lazydocker:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>dk", "<cmd>lua _LAZYDOCKER_TOGGLE()<CR>", { noremap = true, silent = true })
+
+-- create a new terminal with the specified command `bt`,
+-- a shorthand alias for sequentially running `bazel build //...`
+-- followed by `bazel test //...`
+local bazel_build_test = Terminal.new { cmd = "'bazel build ...'", hidden = true }
+
+function _BAZEL_BUILD_TEST()
+    bazel_build_test:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>bt", "<cmd>lua _BAZEL_BUILD_TEST()<CR>", { noremap = true, silent = true })
