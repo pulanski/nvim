fa66009a0c160d5a5466703c391064f1cd19e7e7
pulanski <joshuakersey50@gmail.com>
2022-10-16 21:25:28 -0400

    refactor(plugins): refactor plugins in favor of clear organization
    refactor ordering of plugins in favor of clear organization (i.e. editor enhancements, ide features, etc.)

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -51,21 +51,10 @@ return packer.startup(function(use)
     -- Lua Development --
     ---------------------
 
-    -- Useful lua functions used by lots of plugins
+    -- useful lua functions used by lots of plugins
     use "nvim-lua/plenary.nvim"
-
-    -- Popup API for neovim (WIP)
+    -- popup API for neovim (WIP)
     use "nvim-lua/popup.nvim"
-
-    --------------
-    -- Comments --
-    --------------
-
-    -- Comments
-    use "numToStr/Comment.nvim"
-
-    -- TODO comments
-    use "folke/todo-comments.nvim"
 
     use "JoosepAlviste/nvim-ts-context-commentstring"
     use "kyazdani42/nvim-web-devicons"
@@ -77,40 +66,175 @@ return packer.startup(function(use)
     use "lewis6991/impatient.nvim"
     use "lukas-reineke/indent-blankline.nvim"
 
-    -- Nvim opening dashboard
+    ------------------
+    -- IDE Features --
+    ------------------
+
+    -- nvim opening dashboard
     use "goolord/alpha-nvim"
-
-    -- Terminal usage in neovim
+    -- terminal usage in neovim
     use "akinsho/toggleterm.nvim"
+    -- notifications manager
+    use "rcarriga/nvim-notify"
+    -- telescope (file search, grep search, etc.)
+    use {
+        "nvim-telescope/telescope.nvim",
+        requires = { { "nvim-lua/plenary.nvim" } },
+    }
 
-    -- Google Keep usage in Neovim
-    use { "stevearc/gkeep.nvim", run = ":UpdateRemotePlugins" }
+    ------------------------
+    -- Editor Enhancement --
+    ------------------------
 
-    -- lightbulb for nvim's builtin lsp
+    -- autopairs, integrates with both cmp and treesitter
+    use "windwp/nvim-autopairs"
+    -- highlights trailing whitespace
+    use "ntpeters/vim-better-whitespace"
+    -- peeks lines of the buffer in non-obtrusive way.
+    -- (e.g. :35 will preview or peek line 35 without jumping and can be cancelled)
+    use "nacro90/numb.nvim"
+    -- zen mode
+    use "folke/zen-mode.nvim"
+    -- cycle buffers with a simple UI
+    use "ghillb/cybu.nvim"
+    -- lightbulb for nvim's builtin lsp (similar ux for vscode)
     use "antoinemadec/FixCursorHold.nvim"
     use {
         "kosayoda/nvim-lightbulb",
         requires = "antoinemadec/FixCursorHold.nvim",
     }
-
+    -- vim/nvim usage cheatsheet (useful for new users)
+    use {
+        "sudormrfbin/cheatsheet.nvim",
+        requires = {
+            { "nvim-telescope/telescope.nvim" },
+            { "nvim-lua/popup.nvim" },
+            { "nvim-lua/plenary.nvim" },
+        },
+    }
+    -- display due dates for dates in simple format
+    -- for dates seen within the current buffer
+    use "NFrid/due.nvim"
+    -- scrollbar for better understanding current location in file
     use "Xuyuanp/scrollbar.nvim"
+    -- enhanced highlighting during search
+    -- (e.g. search for `/foo` and the `n` and `N` keys
+    use "rktjmp/highlight-current-n.nvim"
+    -- highlighting utility
+    use "winston0410/range-highlight.nvim"
+    -- highlights parts of line over current textwidth
+    use "lcheylus/overlength.nvim"
+    -- comments
+    use "numToStr/Comment.nvim"
+    -- comments for todos, fixmes, etc. written in source code
+    use { "folke/todo-comments.nvim", requires = "nvim-lua/plenary.nvim" }
+
+    -- TODO update config
+    use {
+        "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end,
+    }
+
+    ---------
+    -- LSP --
+    ---------
+
+    -- enable LSP
+    use "neovim/nvim-lspconfig"
+    -- package manager for neovim (LSP servers, DAP servers, linters, and formatters)
+    use "williamboman/mason.nvim"
+    use "williamboman/mason-lspconfig.nvim"
+    -- for formatters and linters
+    use "jose-elias-alvarez/null-ls.nvim"
+
+    use "ray-x/lsp_signature.nvim"
+    use "RRethy/vim-illuminate"
+    -- show lsp diagnostics in lines between source text
+    use "https://git.sr.ht/~whynothugo/lsp_lines.nvim"
+    -- tree-like view for symbols in neovim using the lsp
+    use "simrat39/symbols-outline.nvim"
+    -- alternative lsp implementation
+    -- use "glepnir/lspsaga.nvim"
+
+    -----------------
+    -- Completions --
+    -----------------
+
+    -- cmp plugin base plugin
+    use "hrsh7th/nvim-cmp"
+    -- buffer completions
+    use "hrsh7th/cmp-buffer"
+    -- path completions
+    use "hrsh7th/cmp-path"
+    -- cmdline completions
+    use "hrsh7th/cmp-cmdline"
+    -- snippet completions
+    use "saadparwaiz1/cmp_luasnip"
+    use "hrsh7th/cmp-nvim-lsp"
+    use "hrsh7th/cmp-emoji"
+    use "hrsh7th/cmp-nvim-lua"
+    -- use {
+    --  "zbirenbaum/copilot-cmp",
+    --  module = "copilot-cmp",
+    -- }
+
+    --------------
+    -- Snippets --
+    --------------
+
+    --snippet engine
+    use "L3MON4D3/LuaSnip"
+    -- collection of useful snippets
+    use "rafamadriz/friendly-snippets"
 
     use "kevinhwang91/promise-async"
     -- code folding (e.g. hiding function details or boilerplate code from
     -- current editor context)
     use "kevinhwang91/nvim-ufo"
+    -- allows for the toggling of transparent backgrounds
+    use "xiyaowong/nvim-transparent"
+    -- vim.ui improvements
+    use "stevearc/dressing.nvim"
+    -- emoji/icon picker
+    use "ziontee113/icon-picker.nvim"
+
+    -------------------------
+    -- Syntax / Treesitter --
+    -------------------------
+
+    use "nvim-treesitter/nvim-treesitter"
+    use "nvim-treesitter/nvim-treesitter-context"
+    -- autoclose tags for Svelte, JS/JSX/TS/TSX, Vue, etc.
+    use "windwp/nvim-ts-autotag"
+    -- surround selections with delimiter pairs (e.g. `()` or `{}`)
+    use "kylechui/nvim-surround"
+
+    ---------------------------
+    -- Keybindings / Keymaps --
+    ---------------------------
+
+    -- whichkey
+    use "folke/which-key.nvim"
+
+    ---------------
+    -- Utilities --
+    ---------------
 
     -- pasting of images into neovim
     use "ekickx/clipboard-image.nvim"
-
-    -- allows for the toggling of transparent backgrounds
-    use "xiyaowong/nvim-transparent"
-
-    -- vim.ui improvements
-    use "stevearc/dressing.nvim"
-
-    -- emoji/icon picker
-    use "ziontee113/icon-picker.nvim"
+    -- mkdir.nvim
+    use "jghauser/mkdir.nvim"
+    -- motions plugin
+    use "ggandor/leap.nvim"
+    -- toggle between absolute and relative number mode
+    use "sitiom/nvim-numbertoggle"
 
     ------------------
     -- Colorschemes --
@@ -136,167 +260,19 @@ return packer.startup(function(use)
     use "RishabhRD/gruvy"
     use "rockyzhang24/arctic.nvim"
 
-    -------------------------
-    -- Syntax / Treesitter --
-    -------------------------
-
-    use "nvim-treesitter/nvim-treesitter"
-    use "nvim-treesitter/nvim-treesitter-context"
-
-    -- Autoclose tags for Svelte, JS/JSX/TS/TSX, Vue, etc.
-    use "windwp/nvim-ts-autotag"
-
-    -- Surround selections with delimiter pairs (e.g. `()` or `{}`)
-    use "kylechui/nvim-surround"
-
-    use {
-        "sudormrfbin/cheatsheet.nvim",
-        requires = {
-            { "nvim-telescope/telescope.nvim" },
-            { "nvim-lua/popup.nvim" },
-            { "nvim-lua/plenary.nvim" },
-        },
-    }
-
-    ------------------------
-    -- Editor Enhancement --
-    ------------------------
-
-    -- Autopairs, integrates with both cmp and treesitter
-    use "windwp/nvim-autopairs"
-
-    -- Highlights trailing whitespace
-    use "ntpeters/vim-better-whitespace"
-
-    -- peeks lines of the buffer in non-obtrusive way.
-    -- (e.g. :35 will preview or peek line 35 without jumping and can be cancelled)
-    use "nacro90/numb.nvim"
-
-    -- Zen Mode
-    use "folke/zen-mode.nvim"
-
-    ---------------------------
-    -- Keybindings / Keymaps --
-    ---------------------------
-
-    -- Whichkey
-    use "folke/which-key.nvim"
-
-    -- Markdown / LaTeX
+    -- markdown / LaTeX
     use "davidgranstrom/nvim-markdown-preview"
     use "ellisonleao/glow.nvim"
-
-    -- snippets
-    --snippet engine
-    use "L3MON4D3/LuaSnip"
-    -- a bunch of snippets to use
-    use "rafamadriz/friendly-snippets"
-
-    ---------
-    -- LSP --
-    ---------
-
-    -- Enable LSP
-    use "neovim/nvim-lspconfig"
-
-    -- Package Manager for Neovim (LSP servers, DAP servers, linters, and formatters)
-    use "williamboman/mason.nvim"
-    use "williamboman/mason-lspconfig.nvim"
-
-    -- For formatters and linters
-    use "jose-elias-alvarez/null-ls.nvim"
-
-    use "ray-x/lsp_signature.nvim"
-    use "RRethy/vim-illuminate"
-    use "https://git.sr.ht/~whynothugo/lsp_lines.nvim"
-
-    -- tree-like view for symbols in neovim using the lsp
-    use "simrat39/symbols-outline.nvim"
-    -- alternative lsp implementation
-    -- use "glepnir/lspsaga.nvim"
-
-    -----------------
-    -- Completions --
-    -----------------
-
-    -- cmp plugins
-    use "hrsh7th/nvim-cmp"
-    use "hrsh7th/cmp-buffer" -- buffer completions
-    use "hrsh7th/cmp-path" -- path completions
-    use "hrsh7th/cmp-cmdline" -- cmdline completions
-    use "saadparwaiz1/cmp_luasnip" -- snippet completions
-    use "hrsh7th/cmp-nvim-lsp"
-    use "hrsh7th/cmp-emoji"
-    use "hrsh7th/cmp-nvim-lua"
-    -- use {
-    --  "zbirenbaum/copilot-cmp",
-    --  module = "copilot-cmp",
-    -- }
 
     ---------------------------
     -- Programming Languages --
     ---------------------------
 
+    -- set of useful utilities for enhancing
+    -- rust development within neovim
     use "simrat39/rust-tools.nvim"
-
-    -- Compiler Explorer
+    -- compiler explorer
     use "krady21/compiler-explorer.nvim"
-
-    -----------
-    -- Games --
-    -----------
-
-    -- Tetris
-    use "alec-gibson/nvim-tetris"
-    -- Minesweeper
-    use "seandewar/nvimesweeper"
-    -- Killersheep
-    use "seandewar/killersheep.nvim"
-
-    -- Due.nvim
-    use "NFrid/due.nvim"
-
-    -- Telescope
-    use {
-        "nvim-telescope/telescope.nvim",
-        requires = { { "nvim-lua/plenary.nvim" } },
-    }
-
-    -- Notifications Manager
-    use "rcarriga/nvim-notify"
-    use "kevinhwang91/nvim-hlslens"
-    -- Enhanced highlighting during search
-    use "rktjmp/highlight-current-n.nvim"
-    -- Highlighting utility
-    use "winston0410/range-highlight.nvim"
-    -- Highlights parts of line over current textwidth
-    use "lcheylus/overlength.nvim"
-
-    -- Cycle buffers with a simple UI
-    use "ghillb/cybu.nvim"
-
-    -- <leader>s open spotify tui (spotify-tui)
-    -- <leader>k open kubernetes tui (k9s)
-
-    use {
-        "folke/trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons",
-        config = function()
-            require("trouble").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end,
-    }
-
-    -- Bazel dependency
-    use "google/vim-maktaba"
-    -- Execute bazel commands from within neovim
-    use "bazelbuild/vim-bazel"
-
-    -- mkdir.nvim
-    use "jghauser/mkdir.nvim"
 
     ---------
     -- Git --
@@ -321,6 +297,39 @@ return packer.startup(function(use)
         },
     }
 
+    ------------
+    -- Docker --
+    ------------
+
+    -- docker ui within neovim
+    use "skanehira/denops-docker.vim"
+
+    -----------
+    -- Bazel --
+    -----------
+
+    -- Bazel dependency
+    use "google/vim-maktaba"
+    -- Execute bazel commands from within neovim
+    use "bazelbuild/vim-bazel"
+
+    ----------------
+    -- Kubernetes --
+    ----------------
+
+    -- k8s dependency
+    -- allows you to write neovim plugins in deno
+    use "vim-denops/denops.vim"
+    use "vim-denops/denops-helloworld.vim"
+    -- kubernetes ui within neovim
+    use "skanehira/k8s.vim"
+    -- kubernetes YAML snippets and integrations with kubectl
+    use "andrewstuart/vim-kubernetes"
+
+    -- similar to lazygit usage in whichkey
+    -- TODO <leader>s open spotify tui (spotify-tui)
+    -- TODO <leader>k open kubernetes tui (k9s)
+
     -- DAP
     use "mfussenegger/nvim-dap"
     use "rcarriga/nvim-dap-ui"
@@ -339,8 +348,28 @@ return packer.startup(function(use)
     --  end
     -- }
 
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
+    ------------------------------
+    -- Third-party Integrations --
+    ------------------------------
+
+    -- Google Keep usage in Neovim
+    use { "stevearc/gkeep.nvim", run = ":UpdateRemotePlugins" }
+
+    -----------
+    -- Games --
+    -----------
+
+    -- tetris
+    use "alec-gibson/nvim-tetris"
+    -- minesweeper
+    use "seandewar/nvimesweeper"
+    -- killersheep
+    use "seandewar/killersheep.nvim"
+
+    use "kevinhwang91/nvim-hlslens"
+
+    -- automatically set up your configuration after cloning packer.nvim
+    -- put this at the end after all plugins
     if PACKER_BOOTSTRAP then
         require("packer").sync()
     end
